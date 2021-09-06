adc242abd1b06202873aaa19c62de2d53c0faa08
LegerLinar <pavelbesedin97@mail.ru>
2021-09-06 19:46:00 +0700

    add value reader in JS for range slider

--- src/js/scripts/range_slider.js
+++ src/js/scripts/range_slider.js
@@ -1,5 +1,17 @@
 let sliderOne = document.getElementById('slider-1');
-let sliderTwo = document.getElementById('slider-1');
+let sliderTwo = document.getElementById('slider-2');
 let displayValOne = document.getElementById('range1');
 let displayValTwo = document.getElementById('range2');
-let minGap = 0
+let minGap = 0;
+console.log(sliderOne.value)
+function slideOne(){
+  if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+    sliderOne.value = parseInt(sliderTwo.value) - minGap;
+  }
+}
+
+function slideTwo(){
+  if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <=minGap){
+    sliderTwo.value = parseInt(sliderOne.value) + minGap
+  }
+}

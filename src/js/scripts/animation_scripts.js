1a92c3bed507723f055a5f98199e7414ca03e889
LegerLinar <pavelbesedin97@mail.ru>
2021-09-14 03:26:25 +0700

    added buttons functions for guests dropdown

--- src/js/scripts/animation_scripts.js
+++ src/js/scripts/animation_scripts.js
@@ -49,12 +49,38 @@ $(function () {
 
 
 //slideDown
+
 function dropDownerRooms(legend) { //dropdown function
   $(`#dropdown_input_` + legend).click(function () { //toggle slider
     $(`#dropdown__field_` + legend).slideToggle()
     $(`#more_` + legend).toggle()
     $(`#less_` + legend).toggle()
-  });
+  })
+
+
+  //buttons for guests
+  $(`#confirm_` + legend).click(function(){
+    $(`#dropdown__field_` + legend).slideToggle()
+
+  })
+
+  $(`#clear_` + legend).click(function (){
+    firstAmount = 0;
+    secondAmount = 0;
+    thirdAmount = 0;
+    $('#amount_1_' + legend).html(firstAmount)
+    $('#amount_2_' + legend).html(secondAmount)
+    $('#amount_3_' + legend).html(thirdAmount)
+    $(`#dropdown_input_` + legend).val( function (){
+      return ''
+    })
+    $(`#clear_` + legend).css({'display':'none'})
+  })
+
+  $(`#dropdown_input_` + legend).change(function() {
+    $(`#clear_` + legend).toggle()
+  })
+
 
   //variables inside of dropdown
   let firstAmount = 0
@@ -109,39 +135,69 @@ function dropDownerRooms(legend) { //dropdown function
 // Input block values
   $(`.counter_wrap_` + legend).click(function () {
     $(`#dropdown_input_` + legend).val(function () {
-      function firstOption() {
-        if (firstAmount >= 1) {
-          return firstAmount + ' ' + $('#amount_legend_1_' + legend).html()
-        }
-        else {
-          return ' '
-        }
-      }
-      function secondOption() {
-        if (secondAmount >= 1) {
-          return secondAmount + ' ' + $('#amount_legend_2_' + legend).html()
-        }
-        else {
-          return ' '
+      if(legend.includes('rooms')) {
+        function firstOption() {
+          if (firstAmount >= 1) {
+            return firstAmount + ' ' + $('#amount_legend_1_' + legend).html()
+          }
+          else {
+            return ' '
+          }
         }
 
-      }
-      function thirdOption() {
-        if (thirdAmount >= 1) {
-          return thirdAmount + ' ' + $('#amount_legend_3_' + legend).html()
+        function secondOption() {
+          if (secondAmount >= 1) {
+            return secondAmount + ' ' + $('#amount_legend_2_' + legend).html()
+          }
+          else {
+            return ' '
+          }
+
+        }
+
+        function thirdOption() {
+          if (thirdAmount >= 1) {
+            return thirdAmount + ' ' + $('#amount_legend_3_' + legend).html()
+          }
+          else {
+            return ' '
+          }
+        }
+
+        if (firstAmount === 0 && secondAmount === 0 && thirdAmount === 0) {
+          return 'Ничего не выбрано'
         }
         else {
-          return ' '
+          return firstOption() + ' ' + secondOption() + ' ' + thirdOption()
         }
+      } else if(legend.includes('guests')){ //Block for guests output
+
+        // block for clear button
+        if(firstAmount + secondAmount + thirdAmount > 0){
+          $(`#clear_` + legend).css({'display':'inline'})
+        } else {
+          $(`#clear_` + legend).css({'display':'none'})
+        }
+        //
+        if (firstAmount + secondAmount + thirdAmount === 0){
+          return 'Сколько гостей'
+        } else {
+          if(firstAmount + secondAmount + thirdAmount === 1){
+
+            return firstAmount + secondAmount + thirdAmount + ' гость'
+          } else if(firstAmount + secondAmount + thirdAmount > 4){
+
+           return  firstAmount + secondAmount + thirdAmount + ' гостей'
+          }
+          return firstAmount + secondAmount + thirdAmount + ' гостя'
+        }
+
+
       }
-      if (firstOption() == undefined && secondOption() == undefined && thirdOption() == undefined) {
-        return 'Ничего не выбрано'
-      }
-      else {
-        return firstOption() + ' ' + secondOption() + ' ' + thirdOption()
-      }
+
     })
   })
+
 }
 
 dropDownerRooms('roomsDefault');

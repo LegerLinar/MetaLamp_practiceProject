adb2f94fe5954398c7c484c6a37d87dbec1fac8c
LegerLinar <pavelbesedin97@mail.ru>
2021-09-17 06:25:52 +0700

    infiltered airdatepicker

--- src/js/scripts/animation_scripts.js
+++ src/js/scripts/animation_scripts.js
@@ -233,28 +233,63 @@ function likeButton(uniqueId) {
         }
       });
 
-    $('#common_form_input_date_example_1').datepicker({
+
+}
+//datePicker scripts
+
+function rangeDateDropdown(uniqueId) {
+   let myDatePicker = $(`#common_form_input_`+uniqueId).datepicker({
+    toggleSelect: false,
+    position: 'bottom center',
+    // minDate: new Date(),
+    autoClose: false,
+    prevHtml: '<div class="arrow_prev_datepicker"></div>',
+    nextHtml: '<div class="arrow_next_datepicker"></div>',
+    navTitle: {
+      days: '<h3> MM, yyyy</h3>'
+    },
+     multipleDatesSeparator: ' - ',
+    // inline: true,
+     clearButton: true,
+    range: true,
+  }).data('datepicker')
+  $('.datepicker--buttons').replaceWith("<div class='datepicker__ownButtons'><h3 class='datepicker__ownButtons_confirm'>Применить</h3><h3 class='datepicker__ownButtons_clear'>Очистить</h3></div>")
+  $('.datepicker__ownButtons_confirm').addClass( `confirm_`+uniqueId)
+  $('.datepicker__ownButtons_clear').addClass( `clear_`+uniqueId)
+  $(`.clear_`+uniqueId).click(function (){
+    myDatePicker.clear()
+  })
+  $(`.confirm_` + uniqueId).click(function(){
+    myDatePicker.hide()
+
+  })
+}
+  function dateDropdown(uniqueId) {
+    let myDatePicker = $(`#common_form_input_`+uniqueId).datepicker({
       toggleSelect: false,
       position: 'bottom center',
       // minDate: new Date(),
-      autoClose: true,
+      autoClose: false,
       prevHtml: '<div class="arrow_prev_datepicker"></div>',
       nextHtml: '<div class="arrow_next_datepicker"></div>',
-      navTitle:{
+      clearButton: true,
+      navTitle: {
         days: '<h3> MM, yyyy</h3>'
       },
-      inline: true,
-      clearButton: true,
-      range: true,
-
-
+      multipleDatesSeparator: ' - ',
+    }).data('datepicker')
+    $('.datepicker--buttons').replaceWith("<div class='datepicker__ownButtons'><h3 class='datepicker__ownButtons_confirm'>Применить</h3><h3 class='datepicker__ownButtons_clear'>Очистить</h3></div>")
+    $('.datepicker__ownButtons_confirm').addClass( `confirm_`+uniqueId)
+    $('.datepicker__ownButtons_clear').addClass( `clear_`+uniqueId)
+    $(`.clear_`+uniqueId).click(function (){
+      myDatePicker.clear()
+    })
+    $(`.confirm_` + uniqueId).click(function(){
+      myDatePicker.hide()
 
     })
-
-}
-
-
-
+  }
+//*[@id="date_example_2"]
 
 //forms_elements scripts
 dropDownerRooms('roomsDefault');
@@ -269,5 +304,8 @@ likeButton('like_def')
 likeButton('like_checked')
 likeButton('like_checked_comment')
 
-
+dateDropdown('date_example_1')
+  //
+dateDropdown('date_example_2')
+rangeDateDropdown('date-filter_example')
 })

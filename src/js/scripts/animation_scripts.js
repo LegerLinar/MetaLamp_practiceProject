f14db7875426def339e61615bb8883d83ba5e037
LegerLinar <pavelbesedin97@mail.ru>
2021-09-18 07:51:00 +0700

    added signup card

--- src/js/scripts/animation_scripts.js
+++ src/js/scripts/animation_scripts.js
@@ -283,6 +283,40 @@ function rangeDateDropdown(uniqueId) {
       autoClose: false,
       prevHtml: '<div class="arrow_prev_datepicker"></div>',
       nextHtml: '<div class="arrow_next_datepicker"></div>',
+      minDate: new Date(),
+      clearButton: true,
+      navTitle: {
+        days: '<h3> MM, yyyy</h3>'
+      },
+      multipleDatesSeparator: ' - ',
+    }).data('datepicker')
+    $('.datepicker--buttons').replaceWith("<div class='datepicker__ownButtons'><h3 class='datepicker__ownButtons_confirm'>Применить</h3><h3 class='datepicker__ownButtons_clear'>Очистить</h3></div>")
+    $('.datepicker__ownButtons_confirm').addClass(`confirm_` + uniqueId)
+    $('.datepicker__ownButtons_clear').addClass(`clear_` + uniqueId)
+    $(`.clear_` + uniqueId).click(function () {
+      myDatePicker.clear()
+    })
+    $(`.confirm_` + uniqueId).click(function () {
+      myDatePicker.hide()
+    })
+    $(`#common_form_input_` + uniqueId).click(function () {
+      $(`#more_` + uniqueId).css('display', 'none')
+      $(`#less_` + uniqueId).css('display', 'block')
+
+    }).blur(function () {
+      $(`#less_` + uniqueId).css('display', 'none')
+      $(`#more_` + uniqueId).css('display', 'block')
+
+    })
+  }
+  function birthDateDropdown(uniqueId) {
+    let myDatePicker = $(`#common_form_input_` + uniqueId).datepicker({
+      toggleSelect: false,
+      position: 'bottom center',
+      // minDate: new Date(),
+      autoClose: false,
+      prevHtml: '<div class="arrow_prev_datepicker"></div>',
+      nextHtml: '<div class="arrow_next_datepicker"></div>',
       clearButton: true,
       navTitle: {
         days: '<h3> MM, yyyy</h3>'
@@ -330,6 +364,7 @@ function rangeDateDropdown(uniqueId) {
 
   dateDropdown('search_number__arriving_date')
   dateDropdown('search_number__departure_date')
+  birthDateDropdown('signup_birthdate')
 
   dropDowner('search_number__guests_dropdown')
 })

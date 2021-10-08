1cfd4d601dafa53302e3203a037a47228850b34f
LegerLinar <pavelbesedin97@mail.ru>
2021-10-08 23:51:10 +0700

    added final styles for cards

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,12 @@ import '../index.pug'
 import '../scss/main.scss'
 import 'jquery'
 import 'air-datepicker'
-const formElementsScript = require('./pages_functions/form-elements')
-const formElementSliderRange = require('./scripts/range_slider')
-// const formElementPagination = require('./scripts/pagination')
-import './jQuery/pagination'// const cardsScript = require('./pages_functions/cards') //Cards page
-
-// const baseScript = require('./scripts/expander'); // Headers&footers page
-// const activatorScript = require('./pages_functions/headers&footres'); //  Headers&Footers
+//form__elements --------------
+// const formElementsScript = require('./pages_functions/form-elements')
+// const formElementSliderRange = require('./scripts/range_slider')
+// import './jQuery/pagination'
+//form__elements -----
+// const cardsScript = require('./pages_functions/cards') //Cards page
+//headers&footers -------------
+const activatorScript = require('./pages_functions/headers&footres'); //  Headers&Footers
 
